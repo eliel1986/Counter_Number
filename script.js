3533bf8c277bfdf90452014ab2bfa5bfa3d46012
Eliel Silva <79846053+eliel1986@users.noreply.github.com>
2024-09-01 22:00:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-document.getElementById('btn_cont').addEventListener('click', cont);
+document.getElementById("btn_cont").addEventListener("click", cont);
 
 function cont() {
-    let choose_number = Number(document.getElementById('select_number').value);
+	let choose_number = Number(document.getElementById("select_number").value);
 
-    let starting_number = Number(document.getElementById('number_initial').value);
+	let starting_number = Number(document.getElementById("number_initial").value);
 
-    let final_number = Number(document.getElementById('end_number').value);
+	let final_number = Number(document.getElementById("end_number").value);
 
-    let qtd = 0;
+	let qtd = 0;
 
-    for (let i = starting_number; i <= final_number; i++) {
-        let indexString = i.toString();
+	for (let i = starting_number; i <= final_number; i++) {
+		let indexString = i.toString();
 
-        for (let j = 0; j < indexString.length; j++) {
-            if (indexString[j] == choose_number) {
-                qtd++
-            }
-        }
-    }
+		for (let j = 0; j < indexString.length; j++) {
+			if (indexString[j] == choose_number) {
+				qtd++;
+			}
+		}
+	}
 
-    let result = document.getElementById('total').value = qtd;
-
+	if (qtd == 1) {
+		document.getElementById("total").value =
+			qtd + " vez o número " + choose_number;
+	} else {
+		document.getElementById("total").value =
+			qtd + " vezes o número " + choose_number;
+	}
 }
